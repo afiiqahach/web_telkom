1d2eb1ca341cd5265f0a4884b55d7e0234225c40
Fakhirazzahrah Ramadhani <fzzahrahh@gmail.com>
2024-10-14 16:44:47 +0800

    kembalikan sidebar menajdi ada logo dan namanya

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -40,59 +40,58 @@ const Header = () => {
   const handleLoginRedirect = () => {
     router.push('/login'); // Arahkan ke halaman login
   };
+
   return (
-    <div className="fixed top-0 left-0 right-0 bg-white z-10 shadow-md">
-      <div className="flex justify-between items-center px-16 py-2 border-b">
-        {/* Search Input */}
-        <div className="relative w-[440px]">
-          <input 
-            type="text" 
-            placeholder="Search in site" 
-            className="border rounded-lg p-2 pl-10 pr-4 w-full" // Padding left untuk ikon pencarian
-          />
-          <SearchIcon className="absolute left-3 top-2.5 h-5 w-5 text-gray-500" />
-        </div>
+    <div className="flex justify-between items-center px-16 py-2 border-b">
+      {/* Search Input */}
+      <div className="relative w-[440px]">
+        <input 
+          type="text" 
+          placeholder="Search in site" 
+          className="border rounded-lg p-2 pl-10 pr-4 w-full" // Padding left untuk ikon pencarian
+        />
+        <SearchIcon className="absolute left-3 top-2.5 h-5 w-5 text-gray-500" />
+      </div>
 
-        {/* Profil dan Nama */}
-        <div className="relative border border-gray-300 rounded-lg w-60 ">
-          <div
-            className="w-60 h-[58px] relative cursor-pointer flex items-center gap-20 hover:bg-gray-200"
-            onClick={() => setDropdownOpen(!dropdownOpen)} // Toggle dropdown
-          >
-            <div className="flex items-center hover:bg-gray-200 rounded-lg">
-              {/* Ikon Profil */}
-              <UserIcon className="h-10 w-10 text-black ml-2" />
-              <div className="left-[73px] top-[11px] ml-4">
-                <div className="text-[#232738] text-sm font-semibold ">
-                  {user ? (user.displayName || user.email) : "Guest"}
-                </div>
-                <div className="text-[#555353] text-xs font-medium">{user ? "User" : "Admin"}</div>
+      {/* Profil dan Nama */}
+      <div className="relative border border-gray-300 rounded-lg w-60 ">
+        <div
+          className="w-60 h-[58px] relative cursor-pointer flex items-center gap-20 hover:bg-gray-200"
+          onClick={() => setDropdownOpen(!dropdownOpen)} // Toggle dropdown
+        >
+          <div className="flex items-center hover:bg-gray-200 rounded-lg">
+            {/* Ikon Profil */}
+            <UserIcon className="h-10 w-10 text-black ml-2" />
+            <div className="left-[73px] top-[11px] ml-4">
+              <div className="text-[#232738] text-sm font-semibold ">
+                {user ? (user.displayName || user.email) : "Guest"}
               </div>
+              <div className="text-[#555353] text-xs font-medium">{user ? "User" : "Admin"}</div>
             </div>
-            <ChevronDownIcon className="h-5 w-5 text-black ml-2" />
           </div>
-
-          {/* Dropdown Menu */}
-          {dropdownOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-10">
-              {user ? (
-                <button
-                  onClick={handleSignOut} // Fungsi Sign Out
-                  className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
-                >
-                  Sign Out
-                </button>
-              ) : (
-                <button
-                  onClick={handleLoginRedirect} // Fungsi untuk mengarahkan ke login
-                  className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
-                >
-                  Login
-                </button>
-              )}
-            </div>
-          )}
+          <ChevronDownIcon className="h-5 w-5 text-black ml-2" />
         </div>
+
+        {/* Dropdown Menu */}
+        {dropdownOpen && (
+          <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg z-10">
+            {user ? (
+              <button
+                onClick={handleSignOut} // Fungsi Sign Out
+                className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+              >
+                Sign Out
+              </button>
+            ) : (
+              <button
+                onClick={handleLoginRedirect} // Fungsi untuk mengarahkan ke login
+                className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+              >
+                Login
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faChevronRight, faInbox, faList, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faChevronRight,
+  faInbox,
+  faList,
+  faCheckCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/navigation";
 
 const Sidebar = () => {
@@ -13,79 +19,90 @@ const Sidebar = () => {
   const router = useRouter();
 
   const navigateToDashboard = () => {
-    router.push('/')
-  }
-
-  const navigateToTicket = () => {
-    router.push('/ticket')
-  }
+    router.push("/");
+  };
 
   return (
-    <div className='fixed py-1'>
-      <div className="justify-start flex bg-white py-4 px-8">
-        <img src="img/logokecil.png" alt="bot" />
-        <button className="text-2xl font-bold justify-start ps-4"
-        onClick={navigateToDashboard}>ARINA</button>
+    <div className="fixed top-0 left-0 w-64 h-full bg-[#EE2E24] text-white shadow-lg">
+      <div className="flex items-center justify-start p-4 bg-white shadow-sm">
+        <img src="img/logokecil.png" alt="Telkom" className="h-10" />
+        <button
+          className="text-2xl font-bold ps-4 text-[#104C98] hover:text-black"
+          onClick={navigateToDashboard}
+        >
+          ARINA
+        </button>
       </div>
-      <div className="w-72 min-h-screen bg-[#EE2E24] text-white p-4 shadow-lg">
+      <div className="p-4">
         {/* Sidebar items */}
-        <div className="mb-4 p-3 hover:bg-red-600 rounded-md cursor-pointer transition-all duration-300"
-        onClick={navigateToTicket}>
+        <div className="mb-4 p-2 hover:bg-red-500 rounded-md cursor-pointer">
           Ticket
         </div>
 
         {/* Incident Ticket with dropdown */}
-        <div 
-          className="flex items-center mb-4 p-3 hover:bg-red-600 rounded-md cursor-pointer transition-all duration-300" 
-          onClick={() => setIncidentOpen(!incidentOpen)}>
+        <div
+          className="flex items-center mb-4 p-2 hover:bg-red-500 rounded-md cursor-pointer"
+          onClick={() => setIncidentOpen(!incidentOpen)}
+        >
           <span>Incident Ticket</span>
-          <FontAwesomeIcon icon={incidentOpen ? faChevronDown : faChevronRight} className="ml-2" />
+          <FontAwesomeIcon
+            icon={incidentOpen ? faChevronDown : faChevronRight}
+            className="ml-2"
+          />
         </div>
 
         {incidentOpen && (
-          <div className="ml-2 transition-all duration-300">
+          <div className="ml-2">
             {/* Saldo Ticket */}
-            <div 
-              className="flex items-center mb-4 p-3 hover:bg-red-600 rounded-md cursor-pointer transition-all duration-300" 
-              onClick={() => setSaldoOpen(!saldoOpen)}>
-              <button>Saldo Ticket</button>
-              <FontAwesomeIcon icon={saldoOpen ? faChevronDown : faChevronRight} className="ml-2" />
+            <div
+              className="flex items-center mb-4 p-2 hover:bg-red-500 rounded-md cursor-pointer"
+              onClick={() => setSaldoOpen(!saldoOpen)}
+            >
+              <span>Saldo Ticket</span>
+              <FontAwesomeIcon
+                icon={saldoOpen ? faChevronDown : faChevronRight}
+                className="ml-2"
+              />
             </div>
 
             {saldoOpen && (
-              <div className="ml-2 flex flex-col items-start transition-all duration-300">
-                <button className="mb-4 p-3 hover:bg-red-600 rounded-md transition-colors duration-300">
+              <div className="ml-4 flex flex-col items-start">
+                <button className="mb-4 p-2 hover:bg-red-500 rounded-md">
                   <FontAwesomeIcon icon={faInbox} className="mr-2" /> Inbox Ticket Draft
                 </button>
-                <button className="mb-4 p-3 hover:bg-red-600 rounded-md transition-colors duration-300">
+                <button className="mb-4 p-2 hover:bg-red-500 rounded-md">
                   <FontAwesomeIcon icon={faInbox} className="mr-2" /> Inbox Ticket Loker
                 </button>
-                <button className="mb-4 p-3 hover:bg-red-600 rounded-md transition-colors duration-300">
+                <button className="mb-4 p-2 hover:bg-red-500 rounded-md">
                   <FontAwesomeIcon icon={faInbox} className="mr-2" /> Inbox Ticket Pribadi
                 </button>
-                <button className="mb-4 p-3 hover:bg-red-600 rounded-md transition-colors duration-300">
+                <button className="mb-4 p-2 hover:bg-red-500 rounded-md">
                   <FontAwesomeIcon icon={faInbox} className="mr-2" /> Ticket Imbas Gamas
                 </button>
-                <button className="mb-4 p-3 hover:bg-red-600 rounded-md transition-colors duration-300">
+                <button className="mb-4 p-2 hover:bg-red-500 rounded-md">
                   <FontAwesomeIcon icon={faInbox} className="mr-2" /> Inbox Ticket Proactive
                 </button>
               </div>
             )}
 
             {/* Request */}
-            <div 
-              className="flex items-center mb-4 p-3 hover:bg-red-600 rounded-md cursor-pointer transition-all duration-300" 
-              onClick={() => setRequestOpen(!requestOpen)}>
+            <div
+              className="flex items-center mb-4 p-2 hover:bg-red-500 rounded-md cursor-pointer"
+              onClick={() => setRequestOpen(!requestOpen)}
+            >
               <span>Request</span>
-              <FontAwesomeIcon icon={requestOpen ? faChevronDown : faChevronRight} className="ml-2" />
+              <FontAwesomeIcon
+                icon={requestOpen ? faChevronDown : faChevronRight}
+                className="ml-2"
+              />
             </div>
 
             {requestOpen && (
-              <div className="ml-4 transition-all duration-300">
-                <button className="mb-4 p-3 hover:bg-red-600 rounded-md transition-colors duration-300">
+              <div className="ml-4">
+                <button className="mb-4 p-2 hover:bg-red-500 rounded-md">
                   <FontAwesomeIcon icon={faList} className="mr-2" /> All Ticket List
                 </button>
-                <button className="mb-4 p-3 hover:bg-red-600 rounded-md transition-colors duration-300">
+                <button className="mb-4 p-2 hover:bg-red-500 rounded-md">
                   <FontAwesomeIcon icon={faCheckCircle} className="mr-2" /> Closed Ticket List
                 </button>
               </div>
